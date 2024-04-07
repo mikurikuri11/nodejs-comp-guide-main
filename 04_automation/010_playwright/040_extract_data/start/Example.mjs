4c898d8a74d5e1c5418b8c27c80652d637c9e61a
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-04-07 23:44:52 +0900

    ✨ Locatorを使用

--- 04_automation/010_playwright/040_extract_data/start/Example.mjs
+++ 04_automation/010_playwright/040_extract_data/start/Example.mjs
@@ -4,9 +4,17 @@ import { chromium } from "@playwright/test";
   const browser = await chromium.launch({ headless: false, slowMo: 500 });
   const page = await browser.newPage();
   await page.goto("http://localhost:3000");
-  const htmlStr = await page.content();
+  // cssセレクターで要素を取得
+  const pageTitleLocator = page.locator(".navbar-brand");
+  const pageTitle = await pageTitleLocator.innerText();
+  // console.log(pageTitle);
 
-  console.log(htmlStr);
+  // 文字列で要素を取得
+  const textLocator = page.locator("text=名刺管理アプリ");
+  const text = await textLocator.innerText();
+  console.log(text);
+
+  // xpathで要素を取得
 
   await browser.close();
 })();
