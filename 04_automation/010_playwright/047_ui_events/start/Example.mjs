06870388edebb86a5d31ec7a648958690ea5ece0
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-04-08 23:39:41 +0900

    ✨ locatorの使い方学習

--- 04_automation/010_playwright/047_ui_events/start/Example.mjs
+++ 04_automation/010_playwright/047_ui_events/start/Example.mjs
@@ -8,12 +8,20 @@ import { chromium } from "@playwright/test";
   const page = await browser.newPage();
   await page.goto("http://localhost:3000");
 
-  // CSS セレクターで要素を取得
-  const pageTitleLocator = await page.locator(".cards.list-group-item");
-  const parentLocator = await pageTitleLocator.locator('..')
-  const pageTitle = await parentLocator.innerHTML();
-  console.log(pageTitle);
+  await page.waitForTimeout(2000);
 
-  await browser.close();
+  const inputLocator = page.locator('//*[@id="__next"]/div/div[1]/label/input');
+  await inputLocator.type('美');
+
+  const pager3Locator = page.locator('.page-link.page-number >> nth=-1');
+  await pager3Locator.click();
+
+  // CSS セレクターで要素を取得
+  const cardLocator = await page.locator(".cards.list-group-item");
+
+  const cardCount = await cardLocator.count();
+  console.log(cardCount);
+
+  // await browser.close();
 
 })();
