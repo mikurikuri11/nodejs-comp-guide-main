3696d99184922deb5f524e25247b377490149888
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-06-27 17:13:52 +0900

    chore: Update script tag in index.html to use the bundled main.js file

--- 05_frontend/010_browser_module/start/main.js
+++ 05_frontend/010_browser_module/start/main.js
@@ -1,10 +1,10 @@
 // const myModule = require('./sub');
 
-// import { helloFromSub } from "./sub";
+import { helloFromSub } from "./sub";
 import { helloFromSub } from "./sub.js";
 import isOdd from "./node_modules/is-odd/index.js";
 
-// helloFromSub();
+helloFromSub();
 
 const oddy = isOdd(3);
 console.log(oddy);
