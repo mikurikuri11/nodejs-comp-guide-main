dd589cb7118c615c58592647c1829fd630078a75
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-06-28 09:06:58 +0900

    feat: Add HTTP server functionality to handle different paths

--- 06_backend/020_path_control/start/app.mjs
+++ 06_backend/020_path_control/start/app.mjs
@@ -2,7 +2,11 @@ import * as http from "http";
 
 const server = http.createServer(function(req, res) {
     console.log(req.url);
-    res.end('bye');
+    if (req.url === '/hello') {
+        res.end('<script>console.log("frontend")</script>');
+    } else if (req.url === '/bye') {
+        res.end('bye');
+    }
 });
 
 server.listen(8080);
